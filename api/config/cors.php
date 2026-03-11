cf89552a45ae7930034acc9b5331e2db86f39e27
LOBASSE <duboissophie876@gmail.com>
2026-03-11 23:40:20 +0000

    Correction CORS : autorisation de toutes les origines pour Vercel

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,9 +1,14 @@
 <?php
 // Configuration CORS pour autoriser les requêtes du frontend React
-header("Access-Control-Allow-Origin: http://localhost:5173");
+// Autoriser les requêtes depuis n'importe quelle origine (nécessaire pour Vercel + InfinityFree)
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Credentials: true");
+} else {
+    header("Access-Control-Allow-Origin: *");
+}
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
 // Gestion des requêtes preflight OPTIONS
